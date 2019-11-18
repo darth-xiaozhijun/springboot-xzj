03b62eac41a524f6ba85bddfc74bc4c2b16d01fd
darth-xiaozhijun <15717061273@163.com>
2019-11-18 22:05:27 +0800

    SpringBoot thymeleaf：URL表达式

--- src/main/java/com/geekshow/controller/DemoController.java
+++ src/main/java/com/geekshow/controller/DemoController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.geekshow.pojo.Users;
@@ -59,5 +60,11 @@ public class DemoController {
 		request.getSession().setAttribute("sess", "HttpSession");
 		request.getSession().getServletContext().setAttribute("app", "Application");
 		return "index5";
+	}
+	
+	@RequestMapping("/{page}")
+	public String showInfo(@PathVariable String page,Integer id,String name){
+		System.out.println(id+"--"+name);
+		return page;
 	}
 }
