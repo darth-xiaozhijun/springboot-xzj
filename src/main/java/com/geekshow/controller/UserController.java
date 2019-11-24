82d4e43ad0ea7c8cb3c5ce4ba09d7f38c427631f
darth-xiaozhijun <15717061273@163.com>
2019-11-24 22:31:03 +0800

    springboot 服务端数据 数据校验

--- src/main/java/com/geekshow/controller/UserController.java
+++ src/main/java/com/geekshow/controller/UserController.java
@@ -3,15 +3,19 @@ package com.geekshow.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.geekshow.pojo.User;
 import com.geekshow.pojo.Users;
 
 /**
  * SpringBoot整合jsp
- *
+ * SpringBoot 表单数据校验
  *
  */
 @Controller
@@ -30,5 +34,19 @@ public class UserController {
 		model.addAttribute("list", list);
 		//跳转视图
 		return "userList";
+	}
+	
+	/**
+	 * 完成用户添加
+	 *@Valid 开启对Users对象的数据校验
+	 *BindingResult:封装了校验的结果
+	 */
+	@RequestMapping("/save")
+	public String saveUser(@Valid User user,BindingResult result){
+		if(result.hasErrors()){
+			return "add";
+		}
+		System.out.println(user);
+		return "ok";
 	}
 }
---
+++ src/main/java/com/geekshow/pojo/User.java
@@ -0,0 +1,44 @@
+package com.geekshow.pojo;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class User {
+
+	@NotBlank //非空校验
+	private String name;
+	
+	@NotBlank //密码非空校验
+	private String password;
+	
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", password=" + password + ", age=" + age + "]";
+	}
+	
+}
