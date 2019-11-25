612a92770ceabed1f3f19a9933c6b30f6d85ee51
darth-xiaozhijun <15717061273@163.com>
2019-11-25 22:21:17 +0800

    SpringBoot 异常处理 @ExceptionHandler

--- src/main/java/com/geekshow/controller/ExceptionController.java
+++ src/main/java/com/geekshow/controller/ExceptionController.java
@@ -1,7 +1,9 @@
 package com.geekshow.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * SpringBoot处理异常方式一：自定义错误页面
@@ -23,5 +25,18 @@ public class ExceptionController {
 	public String showInfo2(){
 		int a = 10/0;
 		return "index";
+	}
+	
+	/**
+	 * java.lang.ArithmeticException
+	 * 该方法需要返回一个ModelAndView：目的是可以让我们封装异常信息以及视图的指定
+	 * 参数Exception e:会将产生异常对象注入到方法中
+	 */
+	@ExceptionHandler(value={java.lang.ArithmeticException.class})
+	public ModelAndView arithmeticExceptionHandler(Exception e){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("error", e.toString());
+		mv.setViewName("error1");
+		return mv;
 	}
 }
