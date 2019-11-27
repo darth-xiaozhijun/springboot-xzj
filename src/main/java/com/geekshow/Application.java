388a2efc2e5e14aa9858519d0a2d643756f51be1
darth-xiaozhijun <15717061273@163.com>
2019-11-27 16:31:37 +0800

    Spring Boot定时任务 @Scheduled的使用

--- src/main/java/com/geekshow/Application.java
+++ src/main/java/com/geekshow/Application.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * SpringBoot 启动类
@@ -18,6 +18,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @ServletComponentScan //在springBoot启动时会扫描@WebServlet，并将该类实例化
 @MapperScan("com.geekshow.mapper") //@MapperScan 用户扫描MyBatis的Mapper接口
 //@EnableCaching
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
