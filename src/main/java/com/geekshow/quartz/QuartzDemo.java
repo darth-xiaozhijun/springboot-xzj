73ace38a0bd9492d56f2a7c6e08e4d754bbda2fd
darth-xiaozhijun <15717061273@163.com>
2019-11-27 16:58:26 +0800

    Spring Boot定时任务 SpringBoot整合Quartz

---
+++ src/main/java/com/geekshow/quartz/QuartzDemo.java
@@ -0,0 +1,20 @@
+package com.geekshow.quartz;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+/**
+ * Job类
+ *
+ *
+ */
+public class QuartzDemo implements Job {
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println("Execute...."+new Date());
+	}
+
+}
