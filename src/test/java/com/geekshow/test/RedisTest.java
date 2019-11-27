9fcaa054f61e2ee4f00a2126694a3b10289587e2
darth-xiaozhijun <15717061273@163.com>
2019-11-27 15:09:52 +0800

    SpringBoot整合SpringData Redis-存取Java对象

--- src/test/java/com/geekshow/test/RedisTest.java
+++ src/test/java/com/geekshow/test/RedisTest.java
@@ -5,9 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.geekshow.Application;
+import com.geekshow.pojo.Peoples;
+import com.geekshow.pojo.Users;
 
 /**
  * Spring Data Redis测试
@@ -36,5 +39,31 @@ public class RedisTest {
 	public void testGet(){
 		String value = (String)this.redisTemplate.opsForValue().get("redis");
 		System.out.println(value);
+	}
+	
+	/**
+	 * 添加Users对象
+	 */
+	@Test
+	public void testSetUesrs(){
+		
+		Peoples peoples = new Peoples();
+		peoples.setAge(20);
+		peoples.setName("张三丰");
+		peoples.setId(1);
+		//重新设置序列化器
+		this.redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+		this.redisTemplate.opsForValue().set("peoples", peoples);
+	}
+	
+	/**
+	 * 取Users对象
+	 */
+	@Test
+	public void testGetUsers(){
+		//重新设置序列化器
+		this.redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+		Peoples peoples = (Peoples)this.redisTemplate.opsForValue().get("peoples");
+		System.out.println(peoples);
 	}
 }
